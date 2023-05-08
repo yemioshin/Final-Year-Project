a96806a37aeaf71c4c201880964ddb2ae39ee875
yemz-slime4 <yemz417@gmail.com>
2023-05-08 12:40:59 +0100

    Deployed to Google Cloud Platform - App is working

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
+import os
 from overview import create_app
 
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
-    # app.run(host='0.0.0.0')
+    #app.run(debug=False, port=5002)
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
 
     
